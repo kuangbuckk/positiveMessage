2ecf4798679b0163f398832aeb99970c2cfa662e
Buck <kuangbuckk159@gmail.com>
2023-02-06 00:58:41 +0700

    added more Mac quotes

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let button = document.getElementById('messageButtonGenerator');
 let message = document.getElementById('message');
 let getBack = document.getElementById('getBack');
 let container = document.getElementById('getMessages');
-let audio = document.getElementById('audio');
+let musicPlayer = document.getElementById('musicPlayer');
 let macMiller = document.getElementById('macMiller');
 
 function generateRandomNumber(num){
@@ -16,7 +16,9 @@ const collectiveMessage = {
     todo: ['having a cup of coffee', 'going for a walk', 'getting early sleep and healthy meals', 'deleting negative apps', 'starting a new hobby', 'learning how to play a new instrument']
 }
 
-const macMillerQuote = ["No matter where life takes me, find me with a smile. - Mac Miller", "The life you live is more important than the words you speak.- Mac Miller", "Enjoy the best things in your life cuz you aint gonna get to live it twice.- Mac Miller", "Someone told me sleep was the cousin of death and following the dollar finds nothing but stress.- Mac Miller"]
+const macMillerQuote = ["\"No matter where life takes me, find me with a smile.\" - Mac Miller", "\"The life you live is more important than the words you speak.\" - Mac Miller", "\"Enjoy the best things in your life cuz you aint gonna get to live it twice.\" - Mac Miller", "\"Someone told me sleep was the cousin of death and following the dollar finds nothing but stress.\" - Mac Miller",
+                        "\"If I stay in one mindset or place for too long, I get crazy.\" - Mac Miller", "\"They say you waste time asleep, but Im just trying to dream.\" - Mac Miller", "\"Ima do my thing until the day the reaper come for me. You can keep on grilling, Imma smile back.\" - Mac Miller", "\"People change, and things go wrong, but just remember, life goes on.\" - Mac Miller",
+                        "\"Some people need to just stop thinking about everything they do and just do it.\"- Mac Miller", "\"I just like to sing for people who have lost love.\" - Mac Miller", "\"There is a lot of beauty in the world, so go hang out and go be a part of the solution rather than the problem.\" - Mac Miller", "\"I just have always felt as long as I am 100 percent honest, then it's just me. It's a lot easier to sleep at night that way.\" - Mac Miller"]
 function macMillerQuoteGenerator(){
     macMiller.innerHTML = macMillerQuote[generateRandomNumber(macMillerQuote.length)];
 }
@@ -52,6 +54,7 @@ function showMessage(){
     button.style.display = "none";
     getBack.style.display = "inline";
     audio.display = "inline-block";
+    musicPlayer.style.display = "block";
 }
 
 button.addEventListener('click', showMessage);
